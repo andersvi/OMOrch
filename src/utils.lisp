344c33367bfca89624e89d96af23b50e4b8d8604
Anders Vinjar <anders@avinjar.no>
2023-10-10 11:29:25 +0200

    clean up utils.lisp, time-tag

--- src/utils.lisp
+++ src/utils.lisp
@@ -1,19 +1,23 @@
 (in-package om)
 
 (defun time-tag ()
-  (multiple-value-bind (sec min hour date month)
+  (multiple-value-bind (sec min hour date month year day daylight-p zone)
       (get-decoded-time)
-    (format nil "~2,'0D-~2,'0D_~2,'0D~2,'0D~2,'0D" month date hour min sec)))
+    (declare (ignore day daylight-p zone))
+    (format nil "~4,'0D-~2,'0D-~2,'0D_~2,'0D:~2,'0D:~2,'0D" year month date hour min sec)))
 
 ;; 
 ;; orchidea output is [... [..] ..]
 ;;
 
-(defun parse-[-delmited-string-to-lists (string)
+(defun parse-[-delmited-string-to-list (string)
+  "return a n-level list of []-delimited elts in string"
   (let ((list-string (substitute #\) #\] (substitute #\( #\[ string))))
     (with-input-from-string (str list-string)
       (loop for seg = (read str nil nil) then (read str nil nil)
 	      while seg collect seg))))
+
+
 
 ;; from cl-cookbook:
 
