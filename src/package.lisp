8393ccbd6428491923d9294c4613e206d749ff34
Anders Vinjar <anders@avinjar.no>
2023-10-10 11:16:25 +0200

    isolate in separate orchidea-package

---
+++ src/package.lisp
@@ -0,0 +1,18 @@
+(in-package :om)
+
+(defpackage "orchidea"
+  (:nicknames :omorch)
+  (:use :common-lisp))
+
+
+(import '(clos:slot-definition-name 
+          clos:slot-definition-initargs 
+          clos:slot-definition-initform 
+          clos:class-direct-superclasses
+          clos:class-direct-subclasses
+	  clos:finalize-inheritance
+          hcl:class-slots
+          hcl:class-direct-slots
+          hcl:validate-superclass
+          mp:without-interrupts)
+	:omorch)
