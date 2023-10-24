1fe98d95f389101aba88f38466d26ba1bfc17c93
Anders Vinjar <anders@avinjar.no>
2023-10-24 15:19:17 +0200

    orchestration->multi-seq : prescription

--- src/score.lisp
+++ src/score.lisp
@@ -25,5 +25,29 @@
     (orch-output->chord-seq orch)))
 
 
+;;; 
+;;; ORCHESTRATION -> MULTI-SEQ
+;;; 
+;;; set up list of "note-allocators", one per instrument
+;;;
+;;; 	1) setup: one allocator per instrument, to grab and pop relevant note from stack
+;;; 	2) setup: stack w. notes and onset from output
+;;; 	3) for each allocator, loop through stack:
+;;; 
+;;; 	   a) check if this note can be played by this instrument at this onset:
+;;; 
+;;; 	      + if this note's 'onset' is >=  current allocators 'next-possible-onset:
+;;; 
+;;; 		A. collect this note, pop it off the input stack
+;;; 		B. update next-possible-onset = onset + note-duration
+;;; 
+;;; 	      + if not, leave it on stack, for consideration by next allocator
+;;; 
+;;; 	   b) return allocator and modified stack
+;;; 
+;;;	4) finally; return list of chord-seqs, one for each allocator
+;;; 
+
+
 
 
