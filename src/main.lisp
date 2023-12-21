e8a020da799acacc49a012fdab62e7660a27f642
Anders Vinjar <anders@avinjar.no>
2023-12-21 15:59:54 +0100

    move output to separate sub-directory : out-files/omorch/'target-subdir'..

--- src/main.lisp
+++ src/main.lisp
@@ -25,74 +25,67 @@
 	((not *orch-path-to-orchestrate*) (error "orchestrate binary not set, use #'orch-set-path-to-orchestrate"))
 	((not (and (= (om::sample-rate target) 44100) (= (om::sample-size target) 16))) (error "orchestrate: target file must be 44.1k, 16 bit, RIFF")))
 	   
-  (let (
-
-	;; set up various input and output filenames
-	(output-dir (namestring (make-pathname 
-				 :directory (append (pathname-directory om::*om-tmpfiles-folder*)
-						    (if *orch-overwrite-previous-run*
-							(list "omorch")
-							(list (string+ "omorch-" (time-tag)))))))))
-    
-
+  (let* ((target-sound (om::filename target))
+	 (output-basename (pathname-name target-sound))
+	 (output-dir (namestring (make-pathname 
+				  :directory (append (pathname-directory om::*om-tmpfiles-folder*)
+						    
+						     (if *orch-overwrite-previous-run*
+							 (list "omorch")
+							 (list (string+ "omorch-" (time-tag))))
+						     (list output-basename)))))
+	 (output-sound (format nil "~A~A-solution.wav" output-dir output-basename)) ;current orchidea supports only RIFF/wav, 44.1k
+	 (output-orchestration (format nil "~A~A.orchestration.txt" output-dir output-basename))
+	 (config-file-name (namestring (tmp-config-file config)))
+	 (instruments (collect-string-items (instruments config) " ")))
+      
     (om::create-directory output-dir)
+      
+    (progn
+      (print (format nil "output-dir: ~A" output-dir))
+      (print (format nil "config-file: ~A" config-file-name))
+      (print (format nil "output-basename: ~A" output-basename))
+      (print (format nil "instruments: ~A" instruments))
+      (print (format nil "output-orchestration ~A" output-orchestration))
+      (print (format nil "output-sound ~A" output-sound))
 
-    (let* ((target-sound (om::filename target))
-	   (output-basename (pathname-name target-sound))
-	   (output-sound (format nil "~A~A-solution.wav" output-dir output-basename)) ;current orchidea supports only RIFF/wav, 44.1k
-	   (output-orchestration (format nil "~A~A.orchestration.txt" output-dir output-basename))
-	   )
-      
-      
-      (let ((config-file-name (namestring (tmp-config-file config)))
-	    (instruments (collect-string-items (instruments config) " ")))
-	
+      (let ((cmd (format nil "cd ~S && ~S ~S ~S"
+			 output-dir
+			 (namestring *orch-path-to-orchestrate*)
+			 (namestring (om::filename target))
+			 config-file-name
+			 )))
+
+
 
 	(progn
-	  (print (format nil "output-dir: ~A" output-dir))
-	  (print (format nil "config-file: ~A" config-file-name))
-	  (print (format nil "output-basename: ~A" output-basename))
-	  (print (format nil "instruments: ~A" instruments))
-	  (print (format nil "output-orchestration ~A" output-orchestration))
-	  (print (format nil "output-sound ~A" output-sound))
+	  (om::om-cmd-line cmd)
 
-	  (let ((cmd (format nil "cd ~S && ~S ~S ~S"
-			     output-dir
-			     (namestring *orch-path-to-orchestrate*)
-			     (namestring (om::filename target))
-			     config-file-name
-			     )))
-
-
-
-	    (progn
-	      (om::om-cmd-line cmd)
-
-	      (rename-file (string+ output-dir "connection.wav") output-sound)
-	      (rename-file (string+ output-dir "connection.txt") output-orchestration)
+	  (rename-file (string+ output-dir "connection.wav") output-sound)
+	  (rename-file (string+ output-dir "connection.txt") output-orchestration)
     
-	      ;; 
-	      ;; return an orchestration-object
-	      ;; 
+	  ;; 
+	  ;; return an orchestration-object
+	  ;; 
 
 
-	      (let* ((orch-struct (om::om-read-file output-orchestration))
-		     (orch-output (parse-orchidea-output orch-struct))
-		     (output-sound (objfromobjs output-sound (make-instance 'sound)))
-		     ;; (output-instrument-list (collect-string-items instrument-pool " "))
-		     )
+	  (let* ((orch-struct (om::om-read-file output-orchestration))
+		 (orch-output (parse-orchidea-output orch-struct))
+		 (output-sound (objfromobjs output-sound (make-instance 'sound)))
+		 ;; (output-instrument-list (collect-string-items instrument-pool " "))
+		 )
 	    
 	    
-		(make-instance 'orchestration
-			       :target-sound target
-			       :output-sound output-sound
-			       :orch-output orch-output
- 			       :command-line cmd
-			       :config-file config-file-name
-			       ;; needs list of instruments to pass to various score-editors
-			       :instruments instruments
+	    (make-instance 'orchestration
+			   :target-sound target
+			   :output-sound output-sound
+			   :orch-output orch-output
+ 			   :command-line cmd
+			   :config-file config-file-name
+			   ;; needs list of instruments to pass to various score-editors
+			   :instruments instruments
 
-			       )))))))))
+			   )))))))
 
 
 ;; recall 'original' orchestration-instance:
